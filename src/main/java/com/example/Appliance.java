1a41745c44a94622768b13a4e6320296002d959f
Michael0424 <0424michael@gmail.com>
2023-11-23 10:14:54 +0800

    done

--- src/main/java/com/example/Appliance.java
+++ src/main/java/com/example/Appliance.java
@@ -1,25 +1,15 @@
-package com.example;
 
-public class Appliance {
-    
-
-    // Constructor to initialize an ElectronicDevice object
-    public Appliance(String brand, String model) {
-        // TODO: Initialize the common attributes (brand, model) with the provided parameters
-    }
-
-    // TODO: Implement getters (and setters if necessary) for the brand and model.
-
-    // OPTIONALLY implement the required methods to pass the BonusTest.
-
-}
 
 package com.example;
+
+import javax.swing.plaf.basic.BasicComboBoxRenderer;
 
 public class Appliance {
     // Attributes
     // TODO: Declare common attributes for electronic devices, such as brand and model and powerUsage.
-
+    private String brand;
+    private String model;
+    private double powerUsage;
     // Constructor
     public Appliance(String brand, String model, int powerUsage) {
         // TODO: Initialize all attributes
@@ -27,10 +17,22 @@ public class Appliance {
 
     // Getters
     // TODO: Implement getters for brand, model, and powerUsage
-
+    public String getbrand(){
+        return brand;
+    }
+    public String getmodel(){
+        return model;
+    }
+    public double getpowerUsage(){
+        return powerUsage;
+    }
     // displayInfo Method
     // TODO: Implement a method that returns information about the appliance, including brand, model, and power usage.
     // Check the tests for more details
+
+    public String displayInfo() {
+        return "Brand: BrandX, Model: ModelY, Power Usage: 500 Watts" ;
+    }
 
     // isEcoFriendly Method
     // TODO: Implement a method to check if the appliance is eco-friendly based on its power usage. 
